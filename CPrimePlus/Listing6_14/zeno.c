bf9f70cdbc42e3feb4e9bf544ef4fc42f91f8f74
JerryChou <Jerry_Chou_123@163.com>
2016-03-11 11:31:06 +0800

    测试4

--- CPrimePlus/Listing6_14/zeno.c
+++ CPrimePlus/Listing6_14/zeno.c
@@ -6,6 +6,7 @@ int main(void) {
 	double time, power_of_2;
 	int limit;
 
+
 	printf("Enter the number of terms you want: "); 
 	scanf("%d", &limit);
 	for (time = 0, power_of_2 = 1, t_ct = 1; t_ct <= limit; t_ct++, power_of_2 *= 2.0) {
