7f9c159cf9b58670cf7f64ea60195060ae427aad
JerryChou <Jerry_Chou_123@163.com>
2016-03-11 10:51:11 +0800

    测试

--- CPrimePlus/Listing6_14/zeno.c
+++ CPrimePlus/Listing6_14/zeno.c
@@ -11,6 +11,7 @@ int main(void) {
 	for (time = 0, power_of_2 = 1, t_ct = 1; t_ct <= limit; t_ct++, power_of_2 *= 2.0) {
 		time += 1.0 / power_of_2;
 		printf("time = %f when terms = %d.\n", time, t_ct);
+
 	}
 	
 	getchar();
